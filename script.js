1504d771c70ab3b8f1359a62cbd584789df67ac6
gchristiansun <167200722+gchristiansun@users.noreply.github.com>
2024-08-11 01:19:04 +0700

    rev

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('menu-toggle').addEventListener('click', function() {
         const menu = document.getElementById('menu');
         menu.classList.toggle('hidden');
+        window.addEventListener('scroll', function() {
+            if (window.scrollY > 1) {
+                menu.classList.add('hidden')
+            }
+
+        })
     });
 })
+
